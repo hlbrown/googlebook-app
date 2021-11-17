e192d41d41cb800968c4b5e431700d3074d20f6e
Hannah Brown <nagle.hannah1@gmail.com>
2021-11-17 16:58:54 -0500

    edited the client app file to include apolloclient

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,25 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -15,7 +32,8 @@ function App() {
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
       </>
-    </Router>
+      </Router>
+      </ApolloProvider>
   );
 }
 
